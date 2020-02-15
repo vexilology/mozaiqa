973a40fd1f4f12726bfb7ee1731d8cd051622a6c
vexilology <andreyisback@yandex.ru>
2020-02-15 17:39:06 +0400

    algorithms and other info

---
+++ src/algorithms.py
@@ -0,0 +1,316 @@
+import hashlib
+import sha3
+import base64
+import config
+from Crypto.Hash import MD2
+
+class FoundHash:
+    """use: hashlib, sha3, crypto.hash_md2"""
+
+    def __init__(self, myfile, a, m):
+        self.myfile = myfile
+        self.addhash = m
+        self.myhash = a 
+
+    def md2(self):
+        if self.myhash == "md2":
+            for password in self.myfile:
+                check_pass = MD2.new(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+    def md4(self):
+        if self.myhash == "md4":
+            for password in self.myfile:
+                check_pass = hashlib.new('md4', (password.strip().encode())).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+    def md5(self):
+        if self.myhash == "md5":
+            for password in self.myfile:
+                check_pass = hashlib.md5(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+    def sha_full(self):
+        if self.myhash == "sha1":
+            for password in self.myfile:
+                check_pass = hashlib.sha1(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "sha224":
+            for password in self.myfile:
+                check_pass = hashlib.sha224(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "sha256":
+            for password in self.myfile:
+                check_pass = hashlib.sha256(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "sha384":
+            for password in self.myfile:
+                check_pass = hashlib.sha384(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "sha512":
+            for password in self.myfile:
+                check_pass = hashlib.sha512(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+    def ripemd160(self):
+        if self.myhash == "ripemd160":
+            for password in self.myfile:
+                check_pass = hashlib.new('ripemd160', (password.strip().encode())).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+    def keccak_full(self):
+        if self.myhash == "keccak224":
+            for password in self.myfile:
+                check_pass = sha3.keccak_224(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "keccak256":
+            for password in self.myfile:
+                check_pass = sha3.keccak_256(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "keccak384":
+            for password in self.myfile:
+                check_pass = sha3.keccak_384(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "keccak512":
+            for password in self.myfile:
+                check_pass = sha3.keccak_512(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+    def shake_full(self):
+        if self.myhash == "shake128-256":
+            for password in self.myfile:
+                check_pass = sha3.shake_128(password.strip().encode()).hexdigest(config.OUTPUT32)
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "shake256-512":
+            for password in self.myfile:
+                check_pass = sha3.shake_256(password.strip().encode()).hexdigest(config.OUTPUT64)
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+    def sha3_full(self):
+        if self.myhash == "sha3-224":
+            for password in self.myfile:
+                check_pass = hashlib.sha3_224(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "sha3-256":
+            for password in self.myfile:
+                check_pass = hashlib.sha3_256(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "sha3-384":
+            for password in self.myfile:
+                check_pass = hashlib.sha3_384(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+        elif self.myhash == "sha3-512":
+            for password in self.myfile:
+                check_pass = hashlib.sha3_512(password.strip().encode()).hexdigest()
+                print("Password ---> {}".format(password.strip()))
+                if check_pass == self.addhash:
+                    print("=" * 20)
+                    print("\n[!]Password found ---> {}".format(password))
+                    print("=" * 20)
+                    break
+            else:
+                print("=" * 20)
+                print("[!]Hash not found ---x {}".format(self.addhash))
+                print("=" * 20)
+
+class FoundBinary:
+    """use: base64"""
+
+    def __init__(self, myfile, a, m):
+        self.myfile = myfile
+        self.addhash = m
+        self.myhash = a
+
+    def base32_64(self):
+        if self.myhash == "base32":
+            check_pass = base64.b32decode(self.addhash.encode())
+            print("=" * 20)
+            print("[!]Password found ---> {}".format(check_pass))
+            print("=" * 20)
+
+        elif self.myhash == "base64":
+            check_pass = base64.b64decode(self.addhash.encode())
+            print("=" * 20)
+            print("[!]Password found ---> {}".format(check_pass))
+            print("=" * 20)
+
+    def ascii85(self):
+        if self.myhash == "ascii85":
+            check_pass = base64.a85decode(self.addhash.encode())
+            print("=" * 20)
+            print("[!]Password found ---> {}".format(check_pass))
+            print("=" * 20)
+
---
+++ src/passwordinfo.py
@@ -0,0 +1,124 @@
+import math
+import string
+import config
+
+class Passwordinfo:
+    def __init__(self, s):
+        self.a = s
+
+    def finish(self):
+        def complex_password(password):
+            length = len(password)
+            complexity = 0
+            lower = string.ascii_lowercase
+            upper = string.ascii_uppercase
+            nums = string.digits
+            has_lower = False
+            has_upper = False
+            has_sym = False
+            has_num = False
+            has_outwards_sym = False
+            has_outwards_num = False
+            has_outwards_upper = False
+
+            symbols = "".join(c for c in string.printable if not c in lower \
+                    and not c in upper and not c in nums)
+            non_print = "".join(chr(n) for n in range(1, 255) if not chr(n) in string.printable)
+
+            for index in range(0, length):
+                if password[index] in non_print:
+                    return 254 ** x
+                if password[index] in lower:
+                    has_lower = True
+                if password[index] in upper:
+                    if index == 0 or index == (length - 1):
+                        has_outwards_upper = True
+                    else:
+                        has_upper = True
+                if password[index] in symbols:
+                    if index == 0 or index == (length - 1):
+                        has_outwards_sym = True
+                    else:
+                        has_sym = True
+                if password[index] in nums:
+                    if index == 0 or index == (length - 1):
+                        has_outwards_num = True
+                    else:
+                        has_num = True
+            if has_lower:
+                complexity += 26
+            if has_upper:
+                complexity += 26
+                has_outwards_upper = False
+            if has_sym:
+                complexity += 38
+                has_outwards_sym = False
+            if has_num:
+                complexity += 10
+                has_outwards_num = False
+            if complexity == 0: complexity = 1
+            combinations = complexity ** length
+            if has_outwards_num:
+                combinations *= 10
+            if has_outwards_upper:
+                combinations *= 26
+            if has_outwards_sym:
+                combinations *= 38
+            return combinations
+
+        def load_dict(book="/usr/share/dict/words"):
+            with open(book) as f:
+                return f.readlines()
+
+        def min_complex_password(password):
+            return len("".join(set(password))) ** len(password)
+
+        def complex_zero(password):
+            total = 0
+            for x in range(0, len(password)):
+                total += complex_password(password[x:])
+            return total
+
+        def min_zero(password):
+            total = 0
+            for x in range(0, len(password)):
+                total += min_complex_password(password[x:])
+            return total
+
+        def calc_mozaiqa(value):
+            mozaiqa = 0
+            while value > 2:
+                value >>= 1
+                mozaiqa += 1
+            return mozaiqa
+
+        while True:
+            div = 1
+            words = load_dict()
+
+            for x in words:
+                x = x.rstrip("\r\n")
+                if len(x) > config.minstring and self.a.find(x) != -1:
+                    div *= complex_password(x) / len(words)
+                    print("{} sounds like a dictioanry word," \
+                            "dividing your result by {}.".format(x, div))
+                    print("{} is calculated by {} own mozaiqa {} multiplied" \
+                            "by any previous found words divided" \
+                            "by the length of the dictionary {}.".format(div, x, complex_password(x), len(words)))
+
+                b = complex_password(self.a)/div
+                c = min_complex_password(self.a)
+                d = complex_zero(self.a)/div
+                e = min_zero(self.a)
+                f = math.log(complex_password(self.a)/div, 2)
+
+                print("{} would approximately take {} tries to bruteforce knowing" \
+                        "the exact length.".format(self.a, b))
+                print("{} would approximately take {} tries to bruteforce by knowing" \
+                        "the exact character set and length.".format(self.a, c))
+                print("{} would approximately take {} tries to bruteforce without knowing" \
+                        "the exact length or character set.".format(self.a, d))
+                print("{} would approximately take {} tries to bruteforce by knowing" \
+                        "the character set but not the length.".format(self.a, e))
+                print("{} has ~{} bits of mozaiqa.".format(self.a, f))
+                quit()
