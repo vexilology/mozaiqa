d3ef16f869cf84049b3796d7d427704558155bd3
vexilology <andreyisback@yandex.ru>
2020-06-02 00:29:04 +0400

    fix config

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ book_path = "/usr/share/dict/words"
 id_algorithms = {"106040": "md4", "106080": "md5",
     "109020": "sha1", "114020": "sha224", "115020": "sha256",
     "119040": "sha384", "122060": "sha512", "109120": "ripemd160",
-    "11": "blake3s256", "12": "blake2b512"}
+    "11": "blake2s256", "12": "blake2b512"}
