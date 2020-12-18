7d5fe210bade032a6f98ddd9fffcb8bddcbbe6a7
vexilology <andreyisback@yandex.ru>
2020-12-19 00:14:39 +0400

    add json and fixed other

--- src/identifier.py
+++ src/identifier.py
@@ -1,92 +1,191 @@
+import json
 import config
+
+
+# TODO: Create id_algorithms.json
+try:
+    with open("hash_examples.json") as h_exm:
+        parse_hash = json.load(h_exm)
+except:
+    print("Cant open json.")
+    exit(1)
 
 class Identifier:
     def __init__(self, i):
         self.ident = i
 
-    def md4(hash, alg_name):
-        t_hash = "a2acde400e61410e79dacbdfc3413151"
+    def md4(hash, a_name):
+        t_hash = parse_hash["md4"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("106040")
+                ) == False and hash.isalnum() == True:
+            a_name.append("1")
 
-    def md5(hash, alg_name):
-        t_hash = "ae11fd697ec92c7c98de3fac23aba525"
+    def md5(hash, a_name):
+        t_hash = parse_hash["md5"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("106080")
+                ) == False and hash.isalnum() == True:
+            a_name.append("2")
 
-    def sha1(hash, alg_name):
-        t_hash = "4a1d4dbc1e193ec3ab2e9213876ceb8f4db72333"
+    def sha1(hash, a_name):
+        t_hash = parse_hash["sha1"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("109020")
+                ) == False and hash.isalnum() == True:
+            a_name.append("3")
 
-    def sha224(hash, alg_name):
-        t_hash = "e301f414993d5ec2bd1d780688d37fe41512f8b57f6923d054ef8e59"
+    def sha224(hash, a_name):
+        t_hash = parse_hash["sha224"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("114020")
+                ) == False and hash.isalnum() == True:
+            a_name.append("4")
 
-    def sha256(hash, alg_name):
-        t_hash = "2c740d20dab7f14ec30510a11f8fd78b82bc3a711abe8a993acdb323e78e6d5e"
+    def sha256(hash, a_name):
+        t_hash = parse_hash["sha256"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("115020")
+                ) == False and hash.isalnum() == True:
+            a_name.append("5")
 
-    def sha384(hash, alg_name):
-        t_hash = "3b21c44f8d830fa55ee9328a7713c6aad548fe6d7a4a438723a0da67c48c485220081a2fbc3e8c17fd9bd65f8d4b4e6b"
+    def sha384(hash, a_name):
+        t_hash = parse_hash["sha384"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("119040")
+                ) == False and hash.isalnum() == True:
+            a_name.append("6")
 
-    def sha512(hash, alg_name):
-        t_hash = "ea8e6f0935b34e2e6573b89c0856c81b831ef2cadfdee9f44eb9aa0955155ba5e8dd97f85c73f030666846773c91404fb0e12fb38936c56f8cf38a33ac89a24e"
+    def sha512(hash, a_name):
+        t_hash = parse_hash["sha512"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("122060")
+                ) == False and hash.isalnum() == True:
+            a_name.append("7")
 
-    def ripemd160(hash, alg_name):
-        t_hash = "dc65552812c66997ea7320ddfb51f5625d74721b"
+    def ripemd160(hash, a_name):
+        t_hash = parse_hash["ripemd160"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("109120")
+                ) == False and hash.isalnum() == True:
+            a_name.append("8")
 
-    def blake2s256(hash, alg_name):
-        t_hash = "6665ec809bc89daf21ec9e77ebac6b817e49673cf7db2bbede072564f84b46d9"
+    def blake2s256(hash, a_name):
+        t_hash = parse_hash["blake2s"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("112055")
+                ) == False and hash.isalnum() == True:
+            a_name.append("9")
 
-    def blake2b512(hash, alg_name):
-        t_hash = "91850ff29412930625ef0723731914104fc9b3b84b62c5e42f43390d8c09fef3618b42f03dfb2e9b9f08b32a5037b00f4c2728bdfa5bb737de56fadc2cbba4be"
+    def blake2b512(hash, a_name):
+        t_hash = parse_hash["blake2b"]
         if len(hash) == len(t_hash) and hash.isalpha(
-            ) == False and hash.isalnum() == True:
-            alg_name.append("112056")
+                ) == False and hash.isalnum() == True:
+            a_name.append("10")
+
+    def sha3_224(hash, a_name):
+        t_hash = parse_hash["sha3_224"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("11")
+
+    def sha3_256(hash, a_name):
+        t_hash = parse_hash["sha3_256"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("12")
+
+    def sha3_384(hash, a_name):
+        t_hash = parse_hash["sha3_384"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("13")
+
+    def sha3_512(hash, a_name):
+        t_hash = parse_hash["sha3_512"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("14")
+
+    def shake128_224(hash, a_name):
+        t_hash = parse_hash["shake128_224"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("15")
+
+    def shake128_256(hash, a_name):
+        t_hash = parse_hash["shake128_256"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("16")
+
+    def shake128_384(hash, a_name):
+        t_hash = parse_hash["shake128_384"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == false and hash.isalnum() == true:
+            a_name.append("17")
+
+    def shake128_512(hash, a_name):
+        t_hash = parse_hash["shake128_512"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("18")
+
+    def shake256_224(hash, a_name):
+        t_hash = parse_hash["shake256_224"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("19")
+
+    def shake256_256(hash, a_name):
+        t_hash = parse_hash["shake256_256"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("20")
+
+    def shake256_384(hash, a_name):
+        t_hash = parse_hash["shake256_384"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == false and hash.isalnum() == true:
+            a_name.append("21")
+
+    def shake256_512(hash, a_name):
+        t_hash = parse_hash["shake256_512"]
+        if len(hash) == len(t_hash) and hash.isalpha(
+                ) == False and hash.isalnum() == True:
+            a_name.append("22")
 
     def timetofound(self):
-        alg_name = []
+        a_name = []
         h_name = self.ident
 
-        Identifier.md4(h_name, alg_name); Identifier.md5(h_name, alg_name);
-        Identifier.sha1(h_name, alg_name); Identifier.sha224(h_name, alg_name);
-        Identifier.sha256(h_name, alg_name); Identifier.sha384(h_name, alg_name);
-        Identifier.sha512(h_name, alg_name); Identifier.ripemd160(h_name, alg_name);
-        Identifier.blake2s256(h_name, alg_name); Identifier.blake2b512(h_name, alg_name)
+        Identifier.md4(h_name, a_name)
+        Identifier.md5(h_name, a_name)
+        Identifier.sha1(h_name, a_name)
+        Identifier.sha224(h_name, a_name)
+        Identifier.sha256(h_name, a_name)
+        Identifier.sha384(h_name, a_name)
+        Identifier.sha512(h_name, a_name)
+        Identifier.ripemd160(h_name, a_name)
+        Identifier.blake2s256(h_name, a_name)
+        Identifier.blake2b512(h_name, a_name)
+        Identifier.sha3_224(h_name, a_name)
+        Identifier.sha3_256(h_name, a_name)
+        Identifier.sha3_384(h_name, a_name)
+        Identifier.sha3_512(h_name, a_name)
+        Identifier.shake128_224(h_name, a_name)
+        Identifier.shake128_256(h_name, a_name)
+        Identifier.shake128_384(h_name, a_name)
+        Identifier.shake128_512(h_name, a_name)
+        Identifier.shake256_224(h_name, a_name)
+        Identifier.shake256_256(h_name, a_name)
+        Identifier.shake256_384(h_name, a_name)
+        Identifier.shake256_512(h_name, a_name)
 
-        if len(alg_name) == 0:
+        if len(a_name) == 0:
             print("[x] Not found.")
             exit(0)
-        elif len(alg_name) > 2:
-            alg_name.sort()
-            print("~"*50)
-            print("[?] -> " + str(config.id_algorithms[alg_name[0]]))
-            print("[?] -> " + str(config.id_algorithms[alg_name[1]]))
-            print("\nLeast Possible:")
-            for a in range(int(len(alg_name)) - 2):
-                print("[?] " + str(config.id_algorithms[alg_name[a + 2]]))
+        elif len(a_name) > 4:
+            a_name.sort()
+            print("[?] :: " + str(config.id_algorithms[a_name[0]]))
+            print("[?] :: " + str(config.id_algorithms[a_name[1]]))
+            print("[?] :: " + str(config.id_algorithms[a_name[2]]))
+            print("[?] :: " + str(config.id_algorithms[a_name[3]]))
+            print("Least Possible:")
+            for a in range(int(len(a_name))-4):
+                print("[?] :: " + str(config.id_algorithms[a_name[a+4]]))
         else:
-            alg_name.sort()
-            print("~"*50)
-            for a in range(len(alg_name)):
-                print("[?] -> " + str(config.id_algorithms[alg_name[a]]))
+            a_name.sort()
+            for a in range(len(a_name)):
+                print("[?] :: " + str(config.id_algorithms[a_name[a]]))
